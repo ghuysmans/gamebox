fb8e12c0b59b31e8b44731334be864d0f8c6bdb7
mlq <mlq@e7175953-5b77-47a5-b69e-39135b4ff788>
2014-03-17 14:22:06 +0000

    Added a smoke test for getLegalMoves() in Connect4
    git-svn-id: https://guigui.dnsdynamic.com:4444/priv_svn/proj_ba1_ghs/trunk@42 e7175953-5b77-47a5-b69e-39135b4ff788

---
+++ code/be/ac/umons/informatique/ba1/gamebox/core/Connect4Test.java
@@ -0,0 +1,31 @@
+
+package be.ac.umons.informatique.ba1.gamebox.core;
+
+import java.util.ArrayList;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author mlq, ghs
+ */
+public class Connect4Test {
+
+	@Test
+	public void legalSmoke() {
+		Game g = new Connect4(7, 6, 4);
+		Player p1 = new HumanPlayer("X", g);
+		g.players.add(p1);
+		
+		ArrayList<Move> mv = g.getLegalMoves(p1);
+		String[] strs = {"PutMove by X@0", "PutMove by X@1", 
+						"PutMove by X@2","PutMove by X@3", 
+						"PutMove by X@4", "PutMove by X@5",
+						"PutMove by X@6"};
+		for (int i=0; i<mv.size(); i++)
+			Assert.assertTrue("Moves match", strs[i].equals(mv.get(i).toString()));
+		Assert.assertEquals("Same length for legal moves", strs.length, mv.size());
+	}
+
+
+}
