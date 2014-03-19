29d270a91f19209fbc1922526577a0c0b05619fa
ghs <ghs@e7175953-5b77-47a5-b69e-39135b4ff788>
2014-03-19 18:20:23 +0000

    Implemented NegamaxAI (yet to be tested)
    git-svn-id: https://guigui.dnsdynamic.com:4444/priv_svn/proj_ba1_ghs/trunk@60 e7175953-5b77-47a5-b69e-39135b4ff788

--- code/be/ac/umons/informatique/ba1/gamebox/core/NegamaxAI.java
+++ code/be/ac/umons/informatique/ba1/gamebox/core/NegamaxAI.java
@@ -1,25 +1,82 @@
 package be.ac.umons.informatique.ba1.gamebox.core;
 
+import java.util.ArrayList;
+
 /** 
  * Negamax algorithm implementation.
  * Much inspired from http://chessprogramming.wikispaces.com/Negamax (2014-02-13).
  */
 
 public class NegamaxAI extends AI {
+	
+	protected final int max_recursion;
 
-	public NegamaxAI() {
+	public NegamaxAI(int max_rec) {
 		super("Negamax AI");
+		max_recursion = max_rec;
 	}
-
+	
+	/**
+	 * @param p Current player
+	 * @param rec Recursion level
+	 */
+	private int computeNode(Player p, int rec) {
+		//leaf? did someone win?
+		int score = p.game.getScore(p);
+		if (score==Game.SCORE_LOST || score==Game.SCORE_WON || rec==max_recursion)
+			//no need to compute further, we're done
+			return score;
+		//test possible moves...
+		ArrayList<Move> mvs = p.game.getLegalMoves(p);
+		if (mvs.size() == 0)
+			return score;
+		else
+		{
+			int M = Integer.MIN_VALUE; //fake value which will be overwritten
+			int v; //score for current move
+			Player otherPlayer = (p.game.players[0]==p ? p.game.players[1] : p.game.players[0]);
+			for (Move mv: mvs)
+			{
+				mv.play();
+				v = -computeNode(otherPlayer, rec+1);
+				mv.undo();
+				if (v > M)
+					M = v; //keep the max
+			}
+			return M;
+		}
+	}
+	
+	/**
+	 * Wrapper to the internal method computeNode(Player p, int rec)
+	 */
 	@Override
 	public int computeNode(Player p) {
-		throw new UnsupportedOperationException(); //FIXME
+		return computeNode(p, 0);
 	}
 
 	@Override
 	public Move getBest(Player p) {
-		throw new UnsupportedOperationException(); //FIXME
+		ArrayList<Move> mvs = p.game.getLegalMoves(p);
+		Move bm = null; //best move (to be returned)
+		if (mvs.size() != 0) //can we do something?
+		{
+			int bs = Integer.MIN_VALUE; //fake value which will be overwritten
+			int v; //score for current move
+			Player otherPlayer = (p.game.players[0]==p ? p.game.players[1] : p.game.players[0]);
+			for (Move mv: mvs)
+			{
+				mv.play();
+				v = -computeNode(otherPlayer);
+				mv.undo();
+				if (v > bs)
+				{
+					bs = v; //keep the best move 
+					bm = mv; //... and the associated score
+				}
+			}
+		}
+		return bm;
 	}
-
 
 }
