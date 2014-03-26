7355659a381b49538543aa565664cbaa3a568483
mlq <mlq@e7175953-5b77-47a5-b69e-39135b4ff788>
2014-03-26 11:52:23 +0000

    Added a play() method in the ComputerPlayer class
    git-svn-id: https://guigui.dnsdynamic.com:4444/priv_svn/proj_ba1_ghs/trunk@72 e7175953-5b77-47a5-b69e-39135b4ff788

--- code/be/ac/umons/informatique/ba1/gamebox/core/ComputerPlayer.java
+++ code/be/ac/umons/informatique/ba1/gamebox/core/ComputerPlayer.java
@@ -4,7 +4,7 @@ package be.ac.umons.informatique.ba1.gamebox.core;
  * Non-playable player
  */
 
-public class ComputerPlayer {
+public class ComputerPlayer extends Player {
 
 	protected final AI ai;
 	
@@ -12,13 +12,22 @@ public class ComputerPlayer {
 	 * Selects an AI corresponding to the given level
 	 * @param lvl Difficulty level (0-anything)
 	 */
-	public ComputerPlayer(int lvl) {
+	public ComputerPlayer(String n, Game g, int lvl) {
+		super(n, g);
 		if (lvl==0)
 			ai = new AbsentAI();
 		else if (lvl==1)
 			ai = new RandomAI();
 		else
 			ai = new NegamaxAI(lvl-1);
+	}
+
+	/**
+	 * Plays the best move for the current player
+	 */
+	public void play() {
+		Move mv = ai.getBest(game.getCurrentPlayer());
+		mv.play();
 	}
 
 
