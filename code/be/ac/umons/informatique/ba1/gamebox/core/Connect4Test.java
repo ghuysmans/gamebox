31a96bfbf3687a45561978a938e496a09d736ba7
ghs <ghs@e7175953-5b77-47a5-b69e-39135b4ff788>
2014-03-27 14:23:20 +0000

    Added a test for Connect4's posStr
    git-svn-id: https://guigui.dnsdynamic.com:4444/priv_svn/proj_ba1_ghs/trunk@85 e7175953-5b77-47a5-b69e-39135b4ff788

--- code/be/ac/umons/informatique/ba1/gamebox/core/Connect4Test.java
+++ code/be/ac/umons/informatique/ba1/gamebox/core/Connect4Test.java
@@ -15,5 +15,26 @@ public class Connect4Test {
 		Assert.assertArrayEquals("Moves match", exp, mv);
 	}
 
+	@Test
+	public void posStr() {
+		Game g = new Connect4(3, 2, 4); //a small one
+		Player p1 = new HumanPlayer("X", g);
+		Player p2 = new HumanPlayer("Y", g);
+		g.setPlayers(p1, p2);
+		for (int x=0; x<g.board.getWidth(); x++) {
+			for (int y=g.board.getHeight()-1; y>=0; y--) {
+				String s = g.posToStr(x, y);
+				int[] a = g.strToPos(s);
+				if (y != g.board.getHeight()) {
+					//not full (yet)
+					Assert.assertArrayEquals("posStr consistency "+s, new int[]{x,y}, a);
+					g.createMove(a[0], a[1]).play();
+				}
+				else {
+					Assert.assertNull("posStr full", a);
+				}
+			}
+		}
+	}
 
 }
