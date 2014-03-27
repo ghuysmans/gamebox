fd28f14034c04900d26f62a832c1d4376bf5793e
ghs <ghs@e7175953-5b77-47a5-b69e-39135b4ff788>
2014-03-27 09:30:00 +0000

    Improved the CUI (undo, players name)
    git-svn-id: https://guigui.dnsdynamic.com:4444/priv_svn/proj_ba1_ghs/trunk@77 e7175953-5b77-47a5-b69e-39135b4ff788

--- code/be/ac/umons/informatique/ba1/gamebox/ui/Main.java
+++ code/be/ac/umons/informatique/ba1/gamebox/ui/Main.java
@@ -12,42 +12,61 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		Game g = new Connect4(7, 6, 4);
+		//Game g = new Connect4(7, 6, 4);
+		Game g = new TicTacToe(3, 3, 3);
 		Player p1 = new HumanPlayer("Guillaume", g);
-		Player p2 = new ComputerPlayer("Mathieu", g, 1);
+		Player p2 = new ComputerPlayer("Mathieu", g, 5);
 		g.setPlayers(p1, p2);
+		
 		do {
 			if (g.getCurrentPlayer() instanceof HumanPlayer) {
-				ArrayList<Move> mv = g.getLegalMoves(g.getCurrentPlayer());
-				for (int k=0; k<mv.size(); k++)
-					System.out.println(mv.get(k));
-				System.out.println(g.history);
-				int sel;
+				ArrayList<Move> mvs = g.getLegalMoves(g.getCurrentPlayer());
+				System.out.println(g.board);
+				if (!g.history.empty())
+					System.out.println("Last move: "+g.history.peek());
+				boolean undo = false;
+				Move sel = null;
+				String typed;
 				do {
-					System.out.print("Move (-1 for undo)? ");
-					sel = sc.nextInt();	
-				} while (sel<-1 || sel>mv.size()-1);
-				if (sel == -1)
-					g.history.undo();
+					System.out.print("@"+g.getCurrentPlayer().name+": your move (u for undo)? ");
+					typed = sc.nextLine();
+					if (typed.equals("u"))
+						undo = true;
+					else
+					{
+						sel = g.createMove(typed);
+						if (!mvs.contains(sel))
+							sel = null;
+					}
+				} while (!undo && sel==null);
+				if (undo)
+				{
+					g.history.undo(); //current player
+					g.history.undo(); //other player
+				}
 				else
-					mv.get(sel).play();	
+					//a legal move has been selected
+					sel.play();	
 			}
 			else
+				//it should work (there are no other classes than ComputerPlayer and HumanPlayer)
 				((ComputerPlayer)(g.getCurrentPlayer())).play();
-			System.out.println(g.board);
 		} while (!g.hasFinished());
+		
 		switch (g.getScore(p1)) {
 		case Game.SCORE_DRAW:
 			System.out.println("Draw.");
 			break;
 		case Game.SCORE_LOST:
-			System.out.println("P2 won.");
+			System.out.println(p2.name+" won.");
 			break;
 		case Game.SCORE_WON:
-			System.out.println("P1 won.");
+			System.out.println(p1.name+" won.");
 		}
-		sc.close();
+		System.out.println("Last move: "+g.history.peek());
+		System.out.println(g.board);
 		
+		sc.close();
 	}
 
 }
