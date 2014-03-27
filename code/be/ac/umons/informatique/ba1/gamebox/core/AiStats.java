ba2808582fccb132193fb8a36440fc1bff2dcc6e
mlq <mlq@e7175953-5b77-47a5-b69e-39135b4ff788>
2014-03-27 14:04:02 +0000

    Added the AiStats class
    git-svn-id: https://guigui.dnsdynamic.com:4444/priv_svn/proj_ba1_ghs/trunk@82 e7175953-5b77-47a5-b69e-39135b4ff788

---
+++ code/be/ac/umons/informatique/ba1/gamebox/core/AiStats.java
@@ -0,0 +1,104 @@
+package be.ac.umons.informatique.ba1.gamebox.core;
+
+/**
+ * Allows comparing performances of two ComputerPlayer (AI) 
+ * @author mlq
+ */
+public class AiStats {
+	
+	protected ComputerPlayer a1;
+	protected ComputerPlayer a2;
+	protected int ctWon = 0;
+	protected int ctLost = 0;
+	protected int ctDraw = 0;
+
+	public AiStats (ComputerPlayer a1, ComputerPlayer a2) {
+		this.a1 = a1;
+		this.a2 = a2;
+	}
+	
+	/**
+	 * Runs one test 
+	 * @param g Empty game
+	 */
+	public void playGame(Game g) {
+		g.setPlayers(a1, a2);
+		do {
+			((ComputerPlayer)(g.getCurrentPlayer())).play();
+			System.out.println(g.board);
+		} while (!g.hasFinished());
+		if (g.getScore() == Game.SCORE_WON)
+			ctWon++;
+		else if (g.getScore() == Game.SCORE_LOST)
+			ctLost++;
+		else
+			ctDraw++;
+	}
+	
+	/**
+	 * @return Number of played games
+	 */
+	public int getCount() {
+		return ctWon + ctDraw + ctLost;
+	}
+	/**
+	 * @return Percentage of won games for the first player
+	 */
+	public double getWon() {
+		int t = getCount();
+		if (t==0)
+			return -1;
+		else
+			return (double)ctWon/t;
+	}
+	
+	/**
+	 * @return Percentage of lost games for the first player
+	 */
+	public double getLost() {
+		int t = getCount();
+		if (t==0)
+			return -1;
+		else
+			return (double)ctLost/t;
+	}		
+	
+	/**
+	 * @return Percentage of draw games for the first player
+	 */	
+	public double getDraw() {
+		int t = getCount();
+		if (t==0)
+			return -1;
+		else
+			return (double)ctDraw/t;
+	}
+	
+	public static void main(String[] args) {
+		Game g = new Connect4(7, 6, 4);
+		//Game g = new TicTacToe(3, 3, 3);
+		ComputerPlayer p1 = new ComputerPlayer("LULU", g, 4);
+		ComputerPlayer p2 = new ComputerPlayer("JOJO", g, 1);		
+		AiStats test = new AiStats(p1, p2);
+		test.playGame(g);
+		System.out.println("gagné: "+test.getWon());
+		System.out.println("egalité: "+test.getDraw());
+		System.out.println("perdu: "+test.getLost());
+		
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
