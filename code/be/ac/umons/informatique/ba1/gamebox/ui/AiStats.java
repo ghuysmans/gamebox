0d556344b8137db803e80822db9eb2d0a04f83da
mlq <mlq@e7175953-5b77-47a5-b69e-39135b4ff788>
2014-03-31 14:41:37 +0000

    Added missing imports
    git-svn-id: https://guigui.dnsdynamic.com:4444/priv_svn/proj_ba1_ghs/trunk@97 e7175953-5b77-47a5-b69e-39135b4ff788

--- code/be/ac/umons/informatique/ba1/gamebox/ui/AiStats.java
+++ code/be/ac/umons/informatique/ba1/gamebox/ui/AiStats.java
@@ -1,5 +1,10 @@
 package be.ac.umons.informatique.ba1.gamebox.ui;
 
+import be.ac.umons.informatique.ba1.gamebox.core.ComputerPlayer;
+import be.ac.umons.informatique.ba1.gamebox.core.Connect4;
+import be.ac.umons.informatique.ba1.gamebox.core.Game;
+import be.ac.umons.informatique.ba1.gamebox.core.TicTacToe;
+
 /**
  * Allows comparing performances of two ComputerPlayer (AI)
  */
