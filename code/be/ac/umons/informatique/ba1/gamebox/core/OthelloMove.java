52dc3a9b6255665f9a0f3a95bfa92c421f9d4813
mlq <mlq@e7175953-5b77-47a5-b69e-39135b4ff788>
2014-04-07 18:25:41 +0000

    (ghs) Missing class
    git-svn-id: https://guigui.dnsdynamic.com:4444/priv_svn/proj_ba1_ghs/trunk@105 e7175953-5b77-47a5-b69e-39135b4ff788

---
+++ code/be/ac/umons/informatique/ba1/gamebox/core/OthelloMove.java
@@ -0,0 +1,39 @@
+package be.ac.umons.informatique.ba1.gamebox.core;
+
+/**
+ * Special PutMove for Othello (with TakeMove consequences)
+ */
+
+public class OthelloMove extends PutMove {
+
+	public OthelloMove(Othello g, int x, int y) {
+		super(g, x, y);
+	}
+	
+	@Override
+	protected void internalPlay() {
+		super.internalPlay();
+		game.nextPlayer(); //still the same player!
+		//Play TakeMove consequences
+		for (int i=0; i<7; i++) {
+			int[] vect = game.board.getVector(i);
+			if (((Othello)game).detectOther(x, y, vect[0], vect[1])) {
+				int pX=x+vect[0], pY=y+vect[1];
+				//no need to check for null!
+				while (game.board.getPiece(pX, pY).owner != game.currentPlayer) {
+					Move csq = new TakeMove(game, true, pX, pY);
+					csq.play();
+					pX += vect[0]; pY += vect[1];
+				}
+			}
+		}
+		//We can now select the other player
+		game.nextPlayer();
+	}
+	
+	@Override
+	public String toString() {
+		return "OthelloMove by "+player.name+"@"+game.posToStr(x, y);
+	}
+
+}
