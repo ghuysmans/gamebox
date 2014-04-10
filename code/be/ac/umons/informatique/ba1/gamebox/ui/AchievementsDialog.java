04b2ec65b19cae3d97c791d15fe09375ee4cb4f9
ghs <ghs@e7175953-5b77-47a5-b69e-39135b4ff788>
2014-04-10 15:31:15 +0000

    Implemented a basic AchievementsDialog
    git-svn-id: https://guigui.dnsdynamic.com:4444/priv_svn/proj_ba1_ghs/trunk@140 e7175953-5b77-47a5-b69e-39135b4ff788

--- code/be/ac/umons/informatique/ba1/gamebox/ui/AchievementsDialog.java
+++ code/be/ac/umons/informatique/ba1/gamebox/ui/AchievementsDialog.java
@@ -1,6 +1,8 @@
 package be.ac.umons.informatique.ba1.gamebox.ui;
 
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 import javax.swing.Box;
@@ -10,6 +12,7 @@ import javax.swing.JComboBox;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JProgressBar;
 import javax.swing.JScrollPane;
 
 import be.ac.umons.informatique.ba1.gamebox.core.Achievement;
@@ -17,16 +20,24 @@ import be.ac.umons.informatique.ba1.gamebox.core.Event;
 import be.ac.umons.informatique.ba1.gamebox.core.HumanPlayer;
 import be.ac.umons.informatique.ba1.gamebox.core.Player;
 
-public class AchievementsDialog extends JDialog {
+public class AchievementsDialog extends JDialog implements ActionListener {
 	
 	protected ArrayList<Player> players; //FIXME
 	protected JPanel list;
 	protected JComboBox<Object> cmbPlayer;
 
-	protected void populateList() {
+	protected void populateList(int idx) {
 		list.removeAll();
-		for (Event evt: players.get(0).events)
-			list.add(new AchievementPanel(evt));
+		for (Event evt: players.get(idx).events)
+			if (evt instanceof Achievement)
+				list.add(new AchievementPanel((Achievement)evt));
+		list.revalidate();
+		list.repaint();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		populateList(cmbPlayer.getSelectedIndex());
 	}
 	
 	public AchievementsDialog(Main parent, boolean modal) {
@@ -46,13 +57,13 @@ public class AchievementsDialog extends JDialog {
 		Box sup = Box.createHorizontalBox();
 		sup.add(Box.createHorizontalGlue());
 		sup.add(new JLabel("Profil de joueur : "));
-		sup.add(new ThinCombo<>(players.toArray()));
+		sup.add(cmbPlayer = new ThinCombo<Object>(players.toArray()));
+		cmbPlayer.addActionListener(this);
 		sup.add(Box.createHorizontalGlue());
-		//sup.add(new ThinCombo<>((Player[]) players.toArray()));
 		
 		list = new JPanel();
 		list.setLayout(new BoxLayout(list, BoxLayout.Y_AXIS));
-		populateList();
+		populateList(0);
 		
 		add(sup);
 		add(new JScrollPane(list));
@@ -61,9 +72,19 @@ public class AchievementsDialog extends JDialog {
 	}
 	
 	protected class AchievementPanel extends JPanel {
-		public AchievementPanel(Event evt) {
-			if (evt instanceof Achievement)
-				add(new JButton(evt.toString()));
+		public AchievementPanel(Achievement ach) {
+			add(new Picture(ach.icon ? ach.name : "no_icon", 64, 64));
+			Box txt = Box.createVerticalBox();
+			txt.add(new ZoomedLabel(ach.message, 1.5F));
+			txt.add(new JLabel(ach.description));
+			txt.add(Box.createVerticalStrut(5));
+			JProgressBar pgr = new JProgressBar(0, 1000);
+			pgr.setValue((int)(ach.getCompletion()*1000));
+			pgr.setString(Math.round(ach.getCompletion()*100)+" %");
+			pgr.setStringPainted(true);
+			txt.add(pgr);
+			add(txt);
+			setAlignmentX(LEFT_ALIGNMENT);
 		}
 		
 		@Override
