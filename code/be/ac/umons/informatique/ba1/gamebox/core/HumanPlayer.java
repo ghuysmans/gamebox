f89e1a573eb9e00e986346ee470cdf1617777c61
ghs <ghs@e7175953-5b77-47a5-b69e-39135b4ff788>
2014-04-10 15:29:45 +0000

    Reversed parameters order
    git-svn-id: https://guigui.dnsdynamic.com:4444/priv_svn/proj_ba1_ghs/trunk@138 e7175953-5b77-47a5-b69e-39135b4ff788

--- code/be/ac/umons/informatique/ba1/gamebox/core/HumanPlayer.java
+++ code/be/ac/umons/informatique/ba1/gamebox/core/HumanPlayer.java
@@ -1,5 +1,4 @@
 package be.ac.umons.informatique.ba1.gamebox.core;
-import java.util.ArrayList;
 
 /** 
  * Usable player (from the UI). It also saves achievements...  
@@ -10,9 +9,9 @@ public class HumanPlayer extends Player {
 	public HumanPlayer(Game g, String n) {
 		super(n, g);
 		
-		events.add(new Achievement(this, "Premier mouvement", "Vous avez joué votre premier coup !   --> "+name, "1stmv", 1, true, true, 
+		events.add(new Achievement(this, "Vous avez joué votre premier coup !", "Ma première fois", "1stmv", 1, true, true, 
 			new Condition(this, null, "Mouvement joué", "mv", 1, false)));
-		events.add(new Achievement(this, "Confiance en soi", "Vous avez défait un mouvement...   --> "+name, "1stud", 1, true, true, 
+		events.add(new Achievement(this, "Vous avez défait un mouvement...", "Confiance en soi", "1stud", 1, true, true, 
 			new Condition(this, null, "Mouvement joué", "ud", 1, false)));
 	}
 
