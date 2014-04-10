e9a3ce98568e5cad4c5ee4bd87ac4b747b9d5f75
mlq <mlq@e7175953-5b77-47a5-b69e-39135b4ff788>
2014-04-10 09:24:14 +0000

    Improved GUI
    git-svn-id: https://guigui.dnsdynamic.com:4444/priv_svn/proj_ba1_ghs/trunk@128 e7175953-5b77-47a5-b69e-39135b4ff788

--- code/be/ac/umons/informatique/ba1/gamebox/ui/AiDialog.java
+++ code/be/ac/umons/informatique/ba1/gamebox/ui/AiDialog.java
@@ -3,6 +3,8 @@ package be.ac.umons.informatique.ba1.gamebox.ui;
 import java.util.Dictionary;
 import java.util.Hashtable;
 
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -10,15 +12,21 @@ import javax.swing.JSlider;
 
 
 
+import javax.swing.JTextField;
+
 import be.ac.umons.informatique.ba1.gamebox.core.AI;
 
 public class AiDialog extends JDialog {
 	
-	
+	protected final JTextField jtf;
+
+
 	public AiDialog(JFrame parent, boolean modal) {
 		super(parent, "Choix de l'ia", modal);
-		setSize(500, 400);
+		setSize(400, 200);
 		setLocationRelativeTo(parent);
+		this.setLayout(new BoxLayout(getContentPane(),BoxLayout.Y_AXIS));
+		setResizable(false);
 		JSlider diff = new JSlider(JSlider.HORIZONTAL,AI.LEVEL_MIN, AI.LEVEL_MAX, (AI.LEVEL_MAX+AI.LEVEL_MIN)/2);
 		diff.setMajorTickSpacing(1);
 		diff.setPaintTicks(true);
@@ -28,7 +36,16 @@ public class AiDialog extends JDialog {
 		labelTable.put( new Integer( AI.LEVEL_MAX ), new JLabel("Difficile") );
 		diff.setLabelTable( labelTable );
 		diff.setPaintLabels(true);
+		
+		Box sup = Box.createHorizontalBox();
+		jtf = new JTextField("Valeur par défaut", 20);
+		sup.add(new JLabel("Nom de l'ai : "));
+		sup.add(jtf);
+		this.add(sup);
 		this.add(diff);
+		
+		
+		
 		setVisible(true);
 
 	}
