976c3aaa7ceb6d15f73a51ec9984e5b6f806a390
ghs <ghs@e7175953-5b77-47a5-b69e-39135b4ff788>
2014-04-14 18:30:49 +0000

    Made a real dialog from AiDialog
    git-svn-id: https://guigui.dnsdynamic.com:4444/priv_svn/proj_ba1_ghs/trunk@166 e7175953-5b77-47a5-b69e-39135b4ff788

--- code/be/ac/umons/informatique/ba1/gamebox/ui/AiDialog.java
+++ code/be/ac/umons/informatique/ba1/gamebox/ui/AiDialog.java
@@ -1,10 +1,13 @@
 package be.ac.umons.informatique.ba1.gamebox.ui;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Dictionary;
 import java.util.Hashtable;
 
 import javax.swing.Box;
 import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -18,24 +21,26 @@ import be.ac.umons.informatique.ba1.gamebox.core.AI;
  */
 
 @SuppressWarnings("serial")
-class AiDialog extends JDialog {
-	//FIXME margins
-	
-	protected final JTextField jtf;
-	protected final JSlider diff;
+class AiDialog extends JDialog implements ActionListener {
 
+	//FIXME margins
+	protected static final String DEFAULT_NAME = "Ordinateur"; 
+	
+	protected boolean cancelled;
+	protected final JTextField name = new ThinTextField(DEFAULT_NAME, 20);
+	protected final JSlider diff = new JSlider(JSlider.HORIZONTAL, AI.LEVEL_MIN, AI.LEVEL_MAX, (AI.LEVEL_MAX+AI.LEVEL_MIN)/2);
+	protected final JButton btnOK = new JButton("OK");
+	protected final JButton btnCcl = new JButton("Annuler");
 
 	public AiDialog(JFrame parent, boolean modal) {
 		super(parent, "Choix de l'IA", modal);
-		setSize(400, 100);
+		setSize(400, 150);
 		setLocationRelativeTo(parent);
 		this.setLayout(new BoxLayout(getContentPane(),BoxLayout.Y_AXIS));
 		setResizable(false);
 		
-		diff = new JSlider(JSlider.HORIZONTAL,AI.LEVEL_MIN, AI.LEVEL_MAX, (AI.LEVEL_MAX+AI.LEVEL_MIN)/2);
-		diff.setMajorTickSpacing(1);
+		diff.setMajorTickSpacing(1); diff.setMinorTickSpacing(1);
 		diff.setPaintTicks(true);
-		diff.setMinorTickSpacing(1);
 		Dictionary<Integer, JLabel> labelTable = new Hashtable<Integer, JLabel>();
 		labelTable.put(new Integer(AI.LEVEL_MIN), new JLabel("Facile"));
 		labelTable.put(new Integer(AI.LEVEL_MAX), new JLabel("Difficile"));
@@ -43,14 +48,46 @@ class AiDialog extends JDialog {
 		diff.setPaintLabels(true);
 		
 		Box sup = Box.createHorizontalBox();
-		jtf = new ThinTextField("Valeur par défaut", 20);
-		sup.add(new JLabel("Nom de l'AI : "));
-		sup.add(jtf);
-		this.add(sup);
-		this.add(Box.createVerticalStrut(5));
-		this.add(diff);
+		sup.add(new JLabel("Nom donné à l'IA : "));
+		sup.add(name);
+		
+		Box ctl = Box.createHorizontalBox();
+		
+		btnOK.addActionListener(this);
+		btnCcl.addActionListener(this);
+		ctl.add(btnOK);
+		ctl.add(Box.createHorizontalStrut(10));
+		ctl.add(btnCcl);
+				
+		add(sup);
+		add(Box.createVerticalStrut(5));
+		add(diff);
+		add(ctl);
 		
 		setVisible(true);
+	}
+	
+	public boolean getCancelled() {
+		return cancelled;
+	}
+	
+	public String getName() {
+		String s = name.getText().trim();
+		if (s.equals(""))
+			return DEFAULT_NAME;
+		else
+			return s;
+	}
+	
+	public int getDifficulty() {
+		return diff.getValue();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == btnCcl)
+			cancelled = true;
+		setVisible(false);
 	}
 
 }
