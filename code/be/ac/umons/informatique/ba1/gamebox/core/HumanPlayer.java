7ad407a7544ebc2962e2ea340aae072c5da3e75b
ghs <ghs@e7175953-5b77-47a5-b69e-39135b4ff788>
2014-04-22 09:13:11 +0000

    Added an achievement idea (42 as Othello final score)
    git-svn-id: https://guigui.dnsdynamic.com:4444/priv_svn/proj_ba1_ghs/trunk@196 e7175953-5b77-47a5-b69e-39135b4ff788

--- code/be/ac/umons/informatique/ba1/gamebox/core/HumanPlayer.java
+++ code/be/ac/umons/informatique/ba1/gamebox/core/HumanPlayer.java
@@ -11,14 +11,17 @@ public class HumanPlayer extends Player {
 	public HumanPlayer(String n) {
 		super(n);
 		
-		events.add(new Achievement(this, "Vous avez joué votre premier coup !", "Ma première fois", "1stmv", 1, false, true, 
+		events.add(new Achievement(this, "Jouez votre premier coup !", "Ma première fois", "1stmv", 1, false, true, 
 			new Condition(this, "Mouvement joué", "mv", 1)));
-		events.add(new Achievement(this, "Vous avez gagné votre première partie !", "Le début de la gloire", "1stwin", 1, false, true, 
+		events.add(new Achievement(this, "Gagnez votre première partie !", "Le début de la gloire", "1stwin", 1, false, true, 
 			new Condition(this, "Victoire", "won", 1)));
-		events.add(new Achievement(this, "Vous avez défait un de vos mouvements...", "Manque de confiance en soi", "1stud", 1, false, true, 
+		events.add(new Achievement(this, "Défaites un de vos mouvements...", "Manque de confiance en soi", "1stud", 1, false, true, 
 			new Condition(this, "Mouvement joué", "ud", 1)));
-		events.add(new Achievement(this, "Vous avez gagné 10 parties d'Othello", "Othellier fou", "croth", 1, false, true, 
+		events.add(new Achievement(this, "Gagnez 10 parties d'Othello", "Othellier fou", "croth", 1, false, true, 
 			new Condition(this, "Victoire à Othello", "won-oth", 10)));
+		//TODO ScoreCondition(boolean finalScore...)
+		/*events.add(new Achievement(this, "Gagnez à Othello avec un score de 42", "Réponse à tout", "fourytwo", 1, false, true, 
+			new Condition(this, "Victoire à Othello", "won-oth", 42)));*/
 	}
 
 	/**
