959afbe28287704046c8eb8638e01bbfa06c8f2a
ghs <ghs@e7175953-5b77-47a5-b69e-39135b4ff788>
2014-04-28 22:25:49 +0000

    Added an important feature to MyObservable: temporary observers
    git-svn-id: https://guigui.dnsdynamic.com:4444/priv_svn/proj_ba1_ghs/trunk@215 e7175953-5b77-47a5-b69e-39135b4ff788

--- code/be/ac/umons/informatique/ba1/gamebox/core/MyObservable.java
+++ code/be/ac/umons/informatique/ba1/gamebox/core/MyObservable.java
@@ -3,9 +3,15 @@ package be.ac.umons.informatique.ba1.gamebox.core;
 import java.io.Serializable;
 import java.util.ArrayList;
 
+/**
+ * An Observable-like class with two features:
+ * - classic observers are saved (for Conditions, Achievements, etc.)
+ * - temporary observers aren't (for UI components, which are volatile)  
+ */
 public class MyObservable implements Serializable {
 	
 	private static final long serialVersionUID = -8152887616190177032L;
+	protected transient ArrayList<MyObserver> tmpObservers;
 	protected ArrayList<MyObserver> observers = new ArrayList<MyObserver>();
 	private boolean changed;
 	
@@ -17,10 +23,21 @@ public class MyObservable implements Serializable {
 		observers.add(obs);
 	}
 	
+	public void addTmpObserver(MyObserver obs) {
+		if (tmpObservers == null)
+			tmpObservers = new ArrayList<MyObserver>();
+		tmpObservers.add(obs);
+	}
+	
 	protected void notifyObservers(Object obj) {
-		if (changed)
+		if (changed) {
 			for (MyObserver o: observers)
 				o.update(this, obj);
+			//maybe a UI (or something else) is connected...
+			if (tmpObservers != null)
+				for (MyObserver o: tmpObservers)
+					o.update(this, obj);
+		}
 	}
 
 }
