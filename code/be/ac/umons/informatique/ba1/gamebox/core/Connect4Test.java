3f00e0d62c9fcc12688c2b23b5339b19274a6dc5
ghs <ghs@e7175953-5b77-47a5-b69e-39135b4ff788>
2014-05-07 16:59:53 +0000

    Refactored Connect4Test and implemented posBonusEven/posBonusOdd
    git-svn-id: https://guigui.dnsdynamic.com:4444/priv_svn/proj_ba1_ghs/trunk@284 e7175953-5b77-47a5-b69e-39135b4ff788

--- code/be/ac/umons/informatique/ba1/gamebox/core/Connect4Test.java
+++ code/be/ac/umons/informatique/ba1/gamebox/core/Connect4Test.java
@@ -1,16 +1,27 @@
 package be.ac.umons.informatique.ba1.gamebox.core;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class Connect4Test {
 
+	Game g;
+	Player p1, p2;
+	
+	@Before
+	/**
+	 * Mini Connect4 settings (3x2)
+	 */
+	public void init() {
+		g = new Connect4(3, 2);
+		p1 = new HumanPlayer("X");
+		p2 = new HumanPlayer("Y");
+		g.setPlayers(p1, p2);
+	}
+	
 	@Test
 	public void legalSmoke() {
-		Game g = new Connect4(3, 2, 4); //a small one
-		Player p = new HumanPlayer("X");
-		Player p2 = new HumanPlayer("Y");
-		g.setPlayers(p, p2); //with only one player
 		Object mv[] = g.getLegalMoves().toArray();
 		Object exp[] = {new PutMove(g, 0, 1), new PutMove(g, 1, 1), new PutMove(g, 2, 1)};
 		Assert.assertArrayEquals("Moves match", exp, mv);
@@ -18,10 +29,6 @@ public class Connect4Test {
 
 	@Test
 	public void posStr() {
-		Game g = new Connect4(3, 2, 4); //a small one
-		Player p1 = new HumanPlayer("X");
-		Player p2 = new HumanPlayer("Y");
-		g.setPlayers(p1, p2);
 		for (int x=0; x<g.board.getWidth(); x++) {
 			for (int y=g.board.getHeight()-1; y>=0; y--) {
 				String s = g.posToStr(x, y);
@@ -36,6 +43,32 @@ public class Connect4Test {
 				}
 			}
 		}
+	}
+	
+	@Test
+	public void posBonusOdd() {
+		g.createMove(1, 0).play(false); //p1
+		g.createMove(2, 0).play(false); //p2
+		Assert.assertEquals("Positional bonus odd p1", 1, g.getPositionalBonus(p1));
+		Assert.assertEquals("Positional bonus odd p2", 0, g.getPositionalBonus(p2));
+		g.createMove(2, 1).play(false); //p1
+		g.createMove(1, 1).play(false); //p2
+		Assert.assertEquals("Positional bonus odd p1", 1, g.getPositionalBonus(p1));
+		Assert.assertEquals("Positional bonus odd p2", 1, g.getPositionalBonus(p2));
+	}
+	
+	@Test
+	public void posBonusEven() {
+		g = new Connect4(4, 2);
+		g.setPlayers(p1, p2);
+		g.createMove(1, 0).play(false); //p1
+		g.createMove(2, 0).play(false); //p2
+		Assert.assertEquals("Positional bonus even p1", 1, g.getPositionalBonus(p1));
+		Assert.assertEquals("Positional bonus even p2", 1, g.getPositionalBonus(p2));
+		g.createMove(0, 0).play(false); //p1
+		g.createMove(1, 1).play(false); //p2
+		Assert.assertEquals("Positional bonus even p1", 1, g.getPositionalBonus(p1));
+		Assert.assertEquals("Positional bonus even p2", 2, g.getPositionalBonus(p2));
 	}
 
 }
