15c4010aed7efa0a9888023044981e16dbf632f4
ghs <ghs@e7175953-5b77-47a5-b69e-39135b4ff788>
2014-05-07 21:22:51 +0000

    Made a packaging script and a shortcut; bonus: a fix for GPG on Windows
    git-svn-id: https://guigui.dnsdynamic.com:4444/priv_svn/proj_ba1_ghs/trunk@297 e7175953-5b77-47a5-b69e-39135b4ff788

---
+++ pack.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+./package.sh /c/temp HUYSMANS-LECLERCQ -s
---
+++ package.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+TMPFILE=$2.tgz
+ARCHIVE=arc/$2.`date +%y%m%d-%H%M%S`.tgz
+
+
+#HELP! I can't figure out why the following condition doesn't work...
+#if [ $# -lt 2 ] -o [ "$3" != "" && "$3" != "-s" ]; then 
+if [ $# -lt 2 ]; then 
+    echo "Usage: package.sh dest NAME [-s]"
+    echo "Create a package ready to be zipped & sent to UMONS servers"
+    echo "by Guillaume HUYSMANS, 2014. License: GNU GPL v2"
+    echo ""
+    echo "Arguments:  dest    destination root folder formatted this way:"
+    echo "                    c:\\yourFolder\\ -> /c/yourFolder"
+    echo "            NAME    your name, UPPERCASE"
+    echo "Flags:      -s      sign a .tgz archive using GPG"
+    exit
+fi
+
+
+echo "Cleaning the destination folder..."
+mkdir -p $1/$2
+rm -R $1/$2
+
+echo "Copying source code..."
+mkdir -p $1/$2/code
+cp -R code/be $1/$2/code
+echo "Copying libraries..."
+cp -R lib $1/$2
+echo "Copying resources..."
+cp -R res $1/$2
+echo "Copying the PDF report..."
+cp doc/official_fr/main.pdf $1/$2/rapport.pdf
+echo "Copying the Ant build file..."
+cp build.xml $1/$2
+
+
+if [ "$3" == "-s" ]; then 
+    echo "Archiving the destination directory..."
+    mkdir -p arc
+    pushd $1
+    tar -czf $TMPFILE $2
+    popd
+    mv $1/$TMPFILE $ARCHIVE
+    echo "Signing the archive..."
+    gpg --detach-sign -a $ARCHIVE
+fi
+
+
+echo "Done."
+
