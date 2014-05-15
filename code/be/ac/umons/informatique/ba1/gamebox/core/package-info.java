f30abc2bc23d1b7f0f59204dd8f5cc2ea4f8668c
ghs <ghs@e7175953-5b77-47a5-b69e-39135b4ff788>
2014-05-15 11:27:59 +0000

    Documented packages using package-info files
    git-svn-id: https://guigui.dnsdynamic.com:4444/priv_svn/proj_ba1_ghs/trunk@367 e7175953-5b77-47a5-b69e-39135b4ff788

---
+++ code/be/ac/umons/informatique/ba1/gamebox/core/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Core classes that could be used with another user interface.
+ * JUnit tests are in this package because they sometimes use private methods.
+ */
+
+package be.ac.umons.informatique.ba1.gamebox.core;
---
+++ code/be/ac/umons/informatique/ba1/gamebox/ui/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Swing Graphical User Interface
+ */
+
+package be.ac.umons.informatique.ba1.gamebox.ui;
